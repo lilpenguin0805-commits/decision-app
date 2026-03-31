9f7191d2bbc39fc22b4c320e7f3cc1645bde8d90
lilpenguin0805-commits <lilpenguin0805@gmail.com>
2026-03-31 11:29:48 -0400

    march 31,2026 4th period
    buttons and added js code

---
+++ script.js
@@ -0,0 +1,15 @@
+let Math = document.querySelector(".Math");
+let Random = document.querySelector(".Random");
+let SocialStudies = document.querySelector(".Social Studies");
+let Science = document.querySelector(".Science");
+let English = document.querySelector(".English");
+let MentalHealth= document.querySelector(".Mental Health");
+
+myElement.addEventListener('click', function(e){
+
+});
+
+
+
+
+
